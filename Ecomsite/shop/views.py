bf66bf6c4ff091b5693108142230f3b0796adeb6
Piotr Skowron <skowronpiotrmail@gmail.com>
2024-04-02 15:04:01 +0200

    Adding cart items to database

--- Ecomsite/shop/views.py
+++ Ecomsite/shop/views.py
@@ -34,6 +34,7 @@ def detail(request, id):
 
 def checkout(request):
     if request.method == "POST":
+        items = request.POST.get("items", "")
         name = request.POST.get("name", "")
         email = request.POST.get("email", "")
         address = request.POST.get("address", "")
@@ -41,7 +42,7 @@ def checkout(request):
         state = request.POST.get("state", "")
         zipcode = request.POST.get("zipcode", "")
 
-        order = Order(name=name, email=email, address=address,
+        order = Order(items=items, name=name, email=email, address=address,
                       city=city, state=state, zipcode=zipcode)
         order.save()
 
